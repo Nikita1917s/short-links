70421ac108a0ce285eeda0fe495ac06d00afeb48
“Nikita1917s” <nekit1917@gmail.com>
2022-12-21 17:28:48 +0200

    deploy errors

--- client/src/hooks/message.hook.ts
+++ client/src/hooks/message.hook.ts
@@ -3,7 +3,7 @@ import {useCallback} from 'react'
 export const useMessage = () => {
   return useCallback((text: string | null) => {
     if (text) {
-      window?.M?.toast({ html: text })
+      M?.toast({ html: text })
     }
   }, [])
 }
