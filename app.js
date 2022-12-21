e908469dd0beee00104437f01569629f800da550
“Nikita1917s” <nekit1917@gmail.com>
2022-12-21 17:49:34 +0200

    cross ev

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoute);
 app.use("/api/link", linkRoute);
 
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 if (process.env.NODE_ENV === 'production')  {
   app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
